5b51c6c57ca0a68b721b77073561c488c988d9fb
Oleg Danko <olegdanko@outlook.com>
2023-06-14 08:41:43 +0300

    Collision detection

--- main.cpp
+++ main.cpp
@@ -39,6 +39,14 @@ void main()
 
 
 template<typename T>
+T rand_0_1();
+
+template<>
+float rand_0_1<float>() {
+    return (float)rand() / (float)RAND_MAX;
+}
+
+template<typename T>
 T rand_1_1();
 
 template<>
@@ -72,9 +80,21 @@ struct Bodies {
     std::vector<float> mass;
     std::vector<float> size;
 
+    struct Body{
+        Bodies &bodies;
+        size_t i;
+        glm::vec3& position() { return bodies.position.at(i); }
+        glm::vec3& velocity() { return bodies.velocity.at(i); }
+        float& mass() { return bodies.mass.at(i); }
+        float& size() { return bodies.size.at(i); }
+    };
+
+    Body get(size_t i) { return {*this, i}; }
+
     float mass_to_size(float val) {
         return val/1000.0f;
     }
+
 
     void add(const glm::vec3& pos,
              const glm::vec3& vel,
@@ -89,6 +109,7 @@ struct Bodies {
 struct BodiesCalculator {
     float G = 0.000000001;
     Bodies& bodies;
+
     std::vector<std::pair<size_t, size_t>> pairs;
     BodiesCalculator(Bodies& bodies) : bodies(bodies) {
         calculate_pairs(bodies.position.size());
@@ -101,8 +122,18 @@ struct BodiesCalculator {
     }
 
     glm::vec3 calc_force(glm::vec3& p1, float& m1, glm::vec3& p2, float m2) {
+        auto dist2 = glm::distance2(p1, p2);
+        if(dist2 == 0) return {};
         auto F = G*m1*m2/glm::distance2(p1, p2);
         return glm::normalize(p2-p1) * F;
+    }
+
+    void apply_force(Bodies::Body a, Bodies::Body b, const glm::vec3& force) {
+        if(a.mass()*b.mass() == 0)
+            return;
+
+        a.velocity() += force / a.mass();
+        b.velocity() -= force / b.mass();
     }
 
     void update_velocities() {
@@ -116,10 +147,9 @@ struct BodiesCalculator {
         });
         for(std::tuple<std::pair<size_t, size_t>&, glm::vec3&> pf
              : std::views::zip(pairs, forces)) {
-            auto [a, b] = std::get<0>(pf);
-            auto f = std::get<1>(pf);
-            bodies.velocity.at(a) += f / bodies.mass.at(a);
-            bodies.velocity.at(b) -= f / bodies.mass.at(b);
+            apply_force(bodies.get(std::get<0>(pf).first),
+                        bodies.get(std::get<0>(pf).second),
+                        std::get<1>(pf));
         }
     }
 
@@ -130,9 +160,60 @@ struct BodiesCalculator {
         }
     }
 
+    void detect_collision(Bodies::Body a, Bodies::Body b) {
+        if(a.mass()*b.mass() == 0)
+            return;
+
+        auto dist = a.size() + b.size();
+
+        if(dist*dist < glm::distance2(a.position(), b.position()))
+            return;
+
+        a.velocity() = a.velocity()/b.mass() + b.velocity()/a.mass();
+        a.mass() += b.mass();
+
+        b.position() = glm::vec3{10.0f};
+        b.velocity() = glm::vec3{0.0f};
+        b.mass() = 0;
+    }
+
+    void detect_collisions() {
+        for(auto [a, b] : pairs) {
+            detect_collision(bodies.get(a), bodies.get(b));
+        }
+    }
+
+    void halt_escapers() {
+        for(auto i : std::views::iota((size_t)0, bodies.position.size())) {
+            auto a = bodies.get(i);
+            bool flag = false;
+            if(a.position().x > 1.0) {
+                a.position().x = 1.0;
+                flag = true;
+            }
+            if(a.position().x < -1.0) {
+                a.position().x = -1.0;
+                flag = true;
+            }
+            if(a.position().y > 1.0) {
+                a.position().y = 1.0;
+                flag = true;
+            }
+            if(a.position().y < -1.0) {
+                a.position().y = -1.0;
+                flag = true;
+            }
+
+            if(flag)
+                a.velocity() = glm::vec3{0.0f};
+        }
+    }
+
     void update() {
         update_velocities();
         update_positions();
+        detect_collisions();
+        halt_escapers();
     }
 };
 
@@ -191,7 +272,7 @@ int main()
     Bodies bodies;
 
     for(int i = 0; i < 100; i++) {
-        bodies.add({rand_1_1<glm::vec2>()/5.0f, 0.0f}, {rand_1_1<glm::vec2>()/1000.0f, 0.0f}, 1.0);
+        bodies.add({rand_1_1<glm::vec2>()/2.0f, 0.0f}, {rand_1_1<glm::vec2>()/10000.0f, 0.0f}, rand_0_1<float>());
     }
 
     BodiesCalculator calc(bodies);
